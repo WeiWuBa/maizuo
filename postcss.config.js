d1eae375186c4f2b9dcf05af264bf8269ebfc7b3
liwei <elvmx47@aliyun.com>
2019-08-26 12:14:30 +0800

    px2rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  // 插件的调用
   plugins: {
-    autoprefixer: {}
+    // 自动补全浏览器的私有前缀的功能 display: flex  display: box
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue: 37.5, // 转换的基准数 html 的fontSize大小为37.5
+      propList: ['*']
+    }
   }
 }
+
+
+// 1rem = 37.5px
+// xrem = 210px
